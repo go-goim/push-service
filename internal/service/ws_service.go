84081da0864f1e53916e7e4b5d1dbd48fe0a935f
yusank <yusankurban@gmail.com>
2022-07-17 14:20:50 +0800

    feat: support id

--- internal/service/ws_service.go
+++ internal/service/ws_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"github.com/go-goim/core/pkg/types"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -14,24 +15,24 @@ import (
 	"github.com/go-goim/push-service/internal/app"
 )
 
-func HandleWsConn(ctx context.Context, c *websocket.Conn, uid string) {
+func HandleWsConn(ctx context.Context, c *websocket.Conn, uid *types.ID) {
 	ww := ws.WrapWs(ctx, c, uid)
 	ww.AddPingAction(func() error {
 		return app.GetApplication().Redis.SetEX(context.Background(),
-			consts.GetUserOnlineAgentKey(uid), app.GetAgentIP(), consts.UserOnlineAgentKeyExpire).Err()
+			consts.GetUserOnlineAgentKey(uid.Int64()), app.GetAgentIP(), consts.UserOnlineAgentKeyExpire).Err()
 	})
 	ww.AddCloseAction(func() error {
 		ctx2, cancel := context.WithTimeout(ctx, time.Second)
 		defer cancel()
-		return app.GetApplication().Redis.Del(ctx2, consts.GetUserOnlineAgentKey(uid)).Err()
+		return app.GetApplication().Redis.Del(ctx2, consts.GetUserOnlineAgentKey(uid.Int64())).Err()
 
 	})
 
 	ctx2, cancel := context.WithTimeout(ctx, time.Second)
 	defer cancel()
-	err := app.GetApplication().Redis.Set(ctx2, consts.GetUserOnlineAgentKey(uid), app.GetAgentIP(), consts.UserOnlineAgentKeyExpire).Err()
+	err := app.GetApplication().Redis.Set(ctx2, consts.GetUserOnlineAgentKey(uid.Int64()), app.GetAgentIP(), consts.UserOnlineAgentKeyExpire).Err()
 	if err != nil {
-		log.Error("redis set error", "key", consts.GetUserOnlineAgentKey(uid), "error", err)
+		log.Error("redis set error", "key", consts.GetUserOnlineAgentKey(uid.Int64()), "error", err)
 	}
 
 	_ = ww.Write([]byte("success"))
